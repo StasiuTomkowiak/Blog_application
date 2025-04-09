1e69ea7f5391c4fd21f3859333ee631ae9cda92f
StasiuTomkowiak <tomkowiakstas@gmail.com>
2025-04-09 23:14:13 +0200

    relationship User - Post done

--- blog/src/main/java/com/stasiu/blog/domain/entities/Post.java
+++ blog/src/main/java/com/stasiu/blog/domain/entities/Post.java
@@ -9,10 +9,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 import lombok.*;
@@ -42,6 +46,10 @@ public class Post {
 
     @Column(nullable = false)
     private Integer readingTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
@@ -103,6 +111,11 @@ public class Post {
         LocalDateTime now = LocalDateTime.now();
         this.createdAt = now;
         this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
     }
     
 }
